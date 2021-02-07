f1c01f2ec94ae26863323260fd452c4aef0b8f7e
Leonid Balagura <leonidbalagura@gmail.com>
2021-02-07 15:24:21 +0200

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
+import CustomButton from "../custom-button/custom-button";
 
 import "./collection-item.scss";
 
-const CollectionItem = ({ name, imageUrl, price }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, imageUrl, price } = item;
   return (
     <div className="collection-item">
       <div
@@ -15,8 +19,15 @@ const CollectionItem = ({ name, imageUrl, price }) => {
         <div className="name">{name}</div>
         <div className="price">{price}</div>
       </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (item) => item.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((item) => {
+      if (item.id === cartItemToAdd.id) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
